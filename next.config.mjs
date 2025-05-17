86120cf4504f5d4d184470b5a2fff4d52eba8622
Rajat Payghan <rajat.payghan@gmail.com>
2025-05-17 16:37:42 +0530

    ?? : Next.config.mjs for allowedDevOrigins

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:"export",
-  images: { unoptimized: true }
+  output: 'export',
+  images: { unoptimized: true },
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'local-origin.dev',
+    '*.local-origin.dev',
+  ],
 };
 
 export default nextConfig;
