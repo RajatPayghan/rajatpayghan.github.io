3d96fe232b6324fa7c8d6e9d82e4679ad0e10272
Rajat Payghan <rajat.payghan@gmail.com>
2025-05-18 15:58:26 +0530

    Fix : Cursor size made smaller

--- src/components/layout/smooth-cursor.js
+++ src/components/layout/smooth-cursor.js
@@ -14,7 +14,7 @@ const DefaultCursorSVG = () => {
       height={54}
       viewBox='0 0 50 54'
       fill='none'
-      style={{ scale: 0.5 }}
+      style={{ scale: 0.4 }}
     >
       <g filter='url(#filter0_d_91_7928)'>
         <path
@@ -76,7 +76,7 @@ const PointerCursorSVG = () => {
       height={54}
       viewBox='0 0 50 54'
       fill='none'
-      style={{ scale: 0.75 }}
+      style={{ scale: 0.4 }}
     >
       <g filter='url(#filter0_d_pointer)'>
         <circle cx='25' cy='27' r='12' fill='black' />
