525d346b56edfd87e3aa732d341e6a747156ca25
Rajat Payghan <rajat.payghan@gmail.com>
2025-05-24 07:44:05 +0530

    Chore : Loading refactor

--- src/app/(site)/loading.js
+++ src/app/(site)/loading.js
@@ -1,19 +1,14 @@
 'use client';
 
-import { cn } from '@/constants/utils';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useEffect, useState } from 'react';
 
 export default function Loading() {
   return (
-    <div
-      className={cn(
-        'fixed flex items-center justify-center left-0 top-0 h-screen w-screen z-50',
-        'bg-neutral-50 dark:bg-neutral-950 transition-colors duration-500'
-      )}
-    >
+    <div className='fixed flex items-center justify-center left-0 top-0 h-screen w-screen z-50 bg-neutral-50 dark:bg-neutral-950 font-instrument'>
       <div className='flex flex-col items-center gap-6'>
         <div className='text-center text-neutral-700 dark:text-neutral-300 animate-pulse'>
-          <p className='font-instrument text-lg md:text-4xl'>Rajat Payghan</p>
+          <p className='text-lg md:text-4xl'>Rajat Payghan</p>
         </div>
       </div>
     </div>
