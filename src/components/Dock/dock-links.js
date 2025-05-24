732748c48b472c5a454db08ce3598b133b0173c5
Rajat Payghan <rajat.payghan@gmail.com>
2025-05-24 21:15:30 +0530

    Fix : Dock Links not hover animating

---
+++ src/components/Dock/dock-links.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '@/styles/dock.css';
+import Link from 'next/link';
+import { cn } from '@/constants/utils';
+import { useDockItemHoverEffect } from '../Hooks/useDockItemMouseMove';
+import { usePathname } from 'next/navigation';
+
+export default function DockLinks({ links }) {
+  const pathname = usePathname();
+  useDockItemHoverEffect();
+  return (
+    <>
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className={cn(
+            'dock-item',
+            pathname === link.href && 'dock-item-selected'
+          )}
+        >
+          <div className='dock-item-text'>{link.name}</div>
+        </Link>
+      ))}
+    </>
+  );
+}
--- src/components/Dock/index.js
+++ src/components/Dock/index.js
@@ -1,15 +1,11 @@
 'use client';
 
 import React from 'react';
-import '@/styles/dock.css';
-import Link from 'next/link';
-import { cn } from '@/constants/utils';
-import { useDockItemHoverEffect } from '../Hooks/useDockItemMouseMove';
-import { usePathname } from 'next/navigation';
-import DockContactButton from './contact-button';
-import { ThemeSwitcherButtonIcon } from '../Layout/theme-switcher';
 import { useIsMobile } from '../Hooks/useIsMobile';
-import { BlurFade } from '../BlurFade/blur-fade';
+import { ThemeSwitcherButtonIcon } from '../Layout/theme-switcher';
+import DockContactButton from './contact-button';
+import DockLinks from './dock-links';
+import '@/styles/dock.css';
 
 const links = [
   { name: 'Home', href: '/' },
@@ -17,35 +13,17 @@ const links = [
 ];
 
 export default function Dock() {
-  const pathname = usePathname();
-  useDockItemHoverEffect();
-
   const isMobile = useIsMobile();
-
-  // Wait for hydration and media query resolution
   if (isMobile === null) return null;
-
   const isDesktop = !isMobile;
 
   return (
     <nav className='dock dock-fade-in'>
-      {links.map((link) => (
-        <Link
-          key={link.href}
-          href={link.href}
-          className={cn(
-            'dock-item',
-            pathname === link.href && 'dock-item-selected'
-          )}
-        >
-          <div className='dock-item-text'>{link.name}</div>
-        </Link>
-      ))}
+      <DockLinks links={links} />
 
       {isDesktop && (
         <>
           <div className='h-6 w-[1px] bg-neutral-200 dark:bg-neutral-800' />
-
           <div className='w-9'>
             <ThemeSwitcherButtonIcon />
           </div>
