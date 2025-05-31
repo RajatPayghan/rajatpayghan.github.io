4405ae6e5ea33a0484dc30475c3fc0e0c528113c
Rajat Payghan <rajat.payghan@gmail.com>
2025-05-31 20:07:52 +0530

    Prose : Incremental-Progress

--- src/components/Writing/Post-Components/quote-box.js
+++ src/components/Writing/Post-Components/quote-box.js
@@ -1,6 +1,6 @@
 export default function QuoteBox({ text, subtext }) {
   return (
-    <span className=' w-full flex flex-col items-center gap-8'>
+    <span className=' w-full flex flex-col items-center gap-8 my-8'>
       <div className='w-32 h-px bg-neutral-100 dark:bg-neutral-800/50' />
       <span className='md:w-[80%] font-hanken text-lg md:text-xl italic text-center'>
         {text}
